f1b39188fe522d4141021a5348eb5d02e902bca0
delfos54 <humancaileo@gmail.com>
2026-06-18 05:00:54 -0400

    se elimina una importación redundante en ProductoRepository

--- producto/src/main/java/com/example/coffee/connect/repository/ProductoRepository.java
+++ producto/src/main/java/com/example/coffee/connect/repository/ProductoRepository.java
@@ -1,15 +1,13 @@
 package com.example.coffee.connect.repository;
 
+import com.example.coffee.connect.model.Productos; //
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.coffee.connect.model.Productos;
-
 @Repository
-public interface ProductoRepository extends JpaRepository<Productos, Long >{
+public interface ProductoRepository extends JpaRepository<Productos, Long> {
 
 }
-    
 
 
 
